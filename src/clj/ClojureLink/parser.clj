a2cf34c8f1ec058a9b37b85f67127948f5e5fefc
timsgardner <timsgardner@gmail.com>
2014-07-20 01:30:46 -0400

    improving clojure-grammar-1

--- src/clj/ClojureLink/parser.clj
+++ src/clj/ClojureLink/parser.clj
@@ -1,4 +1,4 @@
-(ns ClojureLink.parser
+(ns clojurelink.parser
   (:use clojure.repl
         clojure.pprint
         clojure.data)
@@ -26,6 +26,8 @@
 (def awful-hardcoded-path-to-grammar
   "/Users/timothygardner/code/ClojureLink/resources/instaparse/clojure.txt")
 
+(def cg1  "/Users/timothygardner/code/ClojureLink/resources/instaparse/clojure-grammar-1.txt")
+
 (def ^:dynamic *clojure-grammar*
   (make-grammar awful-hardcoded-path-to-grammar))
 
@@ -38,6 +40,24 @@
 
 (defn parse-string [s]
   (insta/parse *clojure-grammar* s))
+
+(defn parses-string [s]
+  (insta/parses *clojure-grammar* s))
+
+(defn parses-file
+  ([f]
+     (->> f slurp parses-string))
+  ([f strtln]
+     (parses-string
+       (lines-to-string
+         (subvec (to-line-vec f)
+           strtln))))
+  ([f strtln endln]
+     (parses-string
+       (lines-to-string
+         (subvec (to-line-vec f)
+           strtln
+           endln)))))
 
 (defn parse-file
   ([f]
